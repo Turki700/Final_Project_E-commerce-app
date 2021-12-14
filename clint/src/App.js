1e12a6974c1fd115b789115a6e6b2f45e6ad1f19
Turki700 <Turki.Haqawi.ruh.js@tuwaiq.edu.sa>
2021-12-14 19:57:34 +0300

    start connect (fron and back)

--- clint/src/App.js
+++ clint/src/App.js
@@ -4,12 +4,23 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
+
+  // const user = true
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/products/:category" element={<ProductList />}/>
+        <Route path="/product/:id" element={<Product />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register  />}/>
+      </Routes>
+    </Router>
   );
 }
 
