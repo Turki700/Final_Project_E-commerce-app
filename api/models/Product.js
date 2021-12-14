0adb67eb4acb478b5bd5b1b6337f2b23652ffcb3
Turki700 <Turki.Haqawi.ruh.js@tuwaiq.edu.sa>
2021-12-14 19:56:43 +0300

    routes done

--- api/models/Product.js
+++ api/models/Product.js
@@ -19,14 +19,18 @@ const ProductSchema = new mongoose.Schema({
         type: Array
     },
     size: {
-        type: String
+        type: Array
     },
     color: {
-        type: String
+        type: Array
     },
     price: {
         type: Number,
         required: [true, "Product Price is required"]
+    },
+    inStock: {
+        type: Boolean,
+        default: true
     }
 }, {timestamps: true})
 
--- api/server.js
+++ api/server.js
@@ -7,14 +7,22 @@ app.use(express.json())
 require("dotenv").config()
 // Imports Routes
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 // MongoDB Connection
 mongoose.connect(process.env.DB_URL).then(() => console.log("DB Connected")).catch((err) => console.log("DB Error"))
 
 
 // Routes
+app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
+app.use("/api/cart", cartRoute)
+app.use("/api/orders", orderRoute)
 
 
 
-app.listen(PORT, () => console.log(`Server up to running with Pott ${PORT}`))
+app.listen(PORT, () => console.log(`Server up to running with Port: ${PORT}`))
