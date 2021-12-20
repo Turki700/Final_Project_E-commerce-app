4fd5b1d574a6524137e66cd30b1e9bf46cb7f2df
Turki700 <Turki.Haqawi.ruh.js@tuwaiq.edu.sa>
2021-12-20 14:44:40 +0300

    some change in requestMethods

--- clint/src/requestMethods.js
+++ clint/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BASE_URL = "http://localhost:5000/api/"
-const TOKEN = "skcocmsdkcnk"
+const TOKEN = "fsddf"
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
