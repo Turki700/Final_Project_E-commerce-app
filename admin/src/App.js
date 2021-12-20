458e8482ee40bcae131702826a522d8c6c7682a0
Turki700 <Turki.Haqawi.ruh.js@tuwaiq.edu.sa>
2021-12-20 12:46:49 +0300

    admin dashboard :white_check_mark:

---
+++ admin/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import SideBar from "./components/sidebar/SideBar";
+import TopBar from "./components/topBar/TopBar";
+import './app.css'
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductsList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+
+function App() {
+  return (
+    <Router>
+      <TopBar />
+
+      {/* Other pages Section  */}
+      <div className="container">
+
+        {/* SideBar Section  */}
+        <SideBar />
+
+        <Routes>
+          {/* Home Section  */}
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newProduct" element={<NewProduct />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+
+export default App;
